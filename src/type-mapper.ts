414e1653cef16d63f63e9c06ff559816944874bd
megos <gmegos.27@gmail.com>
2025-11-29 22:51:37 +0900

    Enable esModuleInterop for better module compatibility
    fix `TypeError: Right-hand side of 'instanceof' is not an object` at SnapShotBox#unbox

--- src/type-mapper.ts
+++ src/type-mapper.ts
@@ -1,4 +1,4 @@
-import * as admin from 'firebase-admin';
+import admin from 'firebase-admin';
 
 export type Firestore = FirebaseFirestore.Firestore;
 export type DocumentReference = FirebaseFirestore.DocumentReference;
